950cc860059c6ad811fc25e9177703f2efb20bc7
PedroQuadrat <105371329+PedroQuadrat@users.noreply.github.com>
2023-08-18 11:44:03 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class App {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        String msg;
+        double num1;
+        double num2;
+
+        System.out.println("Escolha uma das opçoes: 1,2,3,4,5");
+        msg = leitor.next();
+        System.out.println(msg);
+
+        leitor.close();
+
+        public static void opçoes(int num) {
+            switch (num)
+        }
+        
+
+        
+        
+    }
+}
